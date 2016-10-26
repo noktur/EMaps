1ac4d8864fcce8f5a19619442582a982aec49d46
RuberBoy <el_negro_ger@hotmail.com>
2016-10-26 18:27:45 -0300

    b

---
+++ Logica/Interfaces/ILogicaLugar.cs
@@ -0,0 +1,16 @@
+﻿using EntidadesCompartidas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logica.Interfaces
+{
+    public interface ILogicaLugar
+    {
+        void AltaLugar(Lugar l);
+        void ModificarLugar(Lugar l);
+        Lugar BuscarLugarxId(int IdLugar, string NombreCiudad, string NombrePais);
+        void BajaLugar(Lugar l);
+    }
+}
